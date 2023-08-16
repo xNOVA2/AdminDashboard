074d4592dbd41edb9a8a2b0dfb87d84d2a4c300e
ali usaid <aliusaid55@gmail.com>
2023-08-16 10:42:33 -0700

    second commit

--- prisma/connection/connection.tsx
+++ prisma/connection/connection.tsx
@@ -1,5 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-export default prisma;
+export default client;
